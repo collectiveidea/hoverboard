735fd48c5185ddd5f1eb6495d4f1539737abff52
Jon Stokes <jon@jonstokes.com>
2017-03-11 13:22:30 -0600

    Added node and devtool debug

--- server/tests/database.test.js
+++ server/tests/database.test.js
@@ -1,27 +1,27 @@
 /* global describe it expect beforeEach */
 
-import Database from '../data/database';
-import { Headline } from '../data/models';
+import Database from '../data/database'
+import { Headline } from '../data/models'
 
 describe('Database', () => {
-  const viewer = Database.getUser('1');
-  const db = Database.withViewer(viewer);
+  const viewer = Database.getUser('1')
+  const db = Database.withViewer(viewer)
 
   beforeEach(() => {
-    Database.reset();
-  });
+    Database.reset()
+  })
 
   describe('getUser', () => {
     it('gets the user by ID', () => {
-      const user = db.getUser('1');
-      expect(user.id).toBe('1');
-      expect(user.email).toBe('jstokes@maxclicky.com');
-    });
-  });
+      const user = db.getUser('1')
+      expect(user.id).toBe('1')
+      expect(user.email).toBe('jstokes@maxclicky.com')
+    })
+  })
 
   describe('getStory', () => {
     it('gets the story for a viewer and ID', () => {
-      const story = db.getStory('1');
+      const story = db.getStory('1')
       const expectedStory = {
         id: '1',
         userId: db.viewer.id,
@@ -29,10 +29,10 @@ describe('Database', () => {
         started: false,
         url: 'http://foo.com/lorem-1',
         focusKeyword: 'tacos'
-      };
-      expect(story).toEqual(expectedStory);
-    });
-  });
+      }
+      expect(story).toEqual(expectedStory)
+    })
+  })
 
   describe('getHeadline', () => {
     it('gets a headline with a rank for a viewer', () => {
@@ -42,83 +42,83 @@ describe('Database', () => {
         storyId: '1',
         text: 'Lorem ipsum dolor sit amet, consectetur',
         userId: '1'
-      };
-      const headline = db.getHeadline('1');
-      expect(headline).toEqual(expectedHeadline);
-    });
-  });
+      }
+      const headline = db.getHeadline('1')
+      expect(headline).toEqual(expectedHeadline)
+    })
+  })
 
   describe('getStories', () => {
     it('gets a list of stories for a user', () => {
-      const expectedStoryIds = ['1', '2', '3', '4', '5'];
-      const storyIds = db.getStories({}).map(s => s.id);
+      const expectedStoryIds = ['1', '2', '3', '4', '5']
+      const storyIds = db.getStories({}).map(s => s.id)
 
-      expect(storyIds).toEqual(expectedStoryIds);
-    });
-  });
+      expect(storyIds).toEqual(expectedStoryIds)
+    })
+  })
 
   describe('getHeadlines', () => {
     it('gets an ordered list of headlines for a story with ranks', () => {
       // Re-order the headlines so that the ranks don't equal IDs and regular order.
-      db.updateHeadlineRank('1', 3);
-      db.updateHeadlineRank('4', 0);
+      db.updateHeadlineRank('1', 3)
+      db.updateHeadlineRank('4', 0)
 
-      const story = db.getStory('1');
-      const headlines = db.getHeadlines({}, story);
+      const story = db.getStory('1')
+      const headlines = db.getHeadlines({}, story)
 
       // Now the test
-      expect(headlines.map(h => db.getHeadlineRank(h.id).rank)).toEqual([0, 1, 2, 3]);
-      expect(headlines.map(h => h.id)).toEqual(['4', '2', '3', '1']);
-    });
-  });
+      expect(headlines.map(h => db.getHeadlineRank(h.id).rank)).toEqual([0, 1, 2, 3])
+      expect(headlines.map(h => h.id)).toEqual(['4', '2', '3', '1'])
+    })
+  })
 
   describe('updateHeadlineRank', () => {
     it('updates a headline rank', () => {
-      db.updateHeadlineRank('1', 6);
-      expect(db.getHeadlineRank('1').rank).toBe(6);
-    });
-  });
+      db.updateHeadlineRank('1', 6)
+      expect(db.getHeadlineRank('1').rank).toBe(6)
+    })
+  })
 
   describe('updateHeadline', () => {
     it('updates a headline, rank included', () => {
-      db.updateHeadline('1', { disabled: true }, 6);
+      db.updateHeadline('1', { disabled: true }, 6)
 
-      const headline = db.getHeadline('1');
-      expect(db.getHeadlineRank('1').rank).toBe(6);
-      expect(headline.disabled).toBe(true);
-    });
+      const headline = db.getHeadline('1')
+      expect(db.getHeadlineRank('1').rank).toBe(6)
+      expect(headline.disabled).toBe(true)
+    })
 
     it('does not step on the headline\'s id', () => {
-      db.updateHeadline('1', { id: '17' });
+      db.updateHeadline('1', { id: '17' })
 
-      const headline = db.getHeadline('1');
-      expect(headline.id).toBe('1');
-    });
+      const headline = db.getHeadline('1')
+      expect(headline.id).toBe('1')
+    })
 
     it('does not add a clientMutationId key to the headline', () => {
-      db.updateHeadline('1', { clientMutationId: '22' });
+      db.updateHeadline('1', { clientMutationId: '22' })
 
-      const headline = db.getHeadline('1');
-      expect(headline.clientMutationId).not.toBeDefined();
-    });
-  });
+      const headline = db.getHeadline('1')
+      expect(headline.clientMutationId).not.toBeDefined()
+    })
+  })
 
   describe('updateHeadlines', () => {
     it('updates a story\'s headlines', () => {
-      const story = db.getStory('1');
+      const story = db.getStory('1')
       const newHeadlineAttrsList = [
         new Headline({ id: '4', text: 'Duis ipsum dolor sit amet, consectetur', storyId: '1', userId: '1', disabled: false }),
         new Headline({ id: '2', text: 'Donec ipsum dolor sit amet, consectetur', storyId: '1', userId: '1', disabled: false }),
         new Headline({ id: '3', text: 'Nulla ipsum dolor sit amet, consectetur', storyId: '1', userId: '1', disabled: true }),
         new Headline({ id: '1', text: 'Lorem ipsum dolor sit amet, consectetur', storyId: '1', userId: '1', disabled: false }),
-      ];
+      ]
 
-      db.updateHeadlines(newHeadlineAttrsList);
+      db.updateHeadlines(newHeadlineAttrsList)
 
-      const headlines = db.getHeadlines({}, story);
+      const headlines = db.getHeadlines({}, story)
 
-      expect(headlines.map(h => h.id)).toEqual(['4', '2', '3', '1']);
-      expect(headlines.map(h => db.getHeadlineRank(h.id).rank)).toEqual([0, 1, 2, 3]);
-    });
-  });
-});
+      expect(headlines.map(h => h.id)).toEqual(['4', '2', '3', '1'])
+      expect(headlines.map(h => db.getHeadlineRank(h.id).rank)).toEqual([0, 1, 2, 3])
+    })
+  })
+})
--- server/tests/modelTypes.test.js
+++ server/tests/modelTypes.test.js
@@ -1,9 +1,9 @@
 /* global describe it expect */
 
-import { graphql } from 'graphql';
-import schema from '../data/schema';
-import Database from '../data/database';
-import { toGlobalId } from 'graphql-relay';
+import { graphql } from 'graphql'
+import schema from '../data/schema'
+import Database from '../data/database'
+import { toGlobalId } from 'graphql-relay'
 
 describe('GraphQL', () => {
   describe('UserType', () => {
@@ -15,19 +15,19 @@ describe('GraphQL', () => {
             name
           }
         }
-      `;
+      `
 
-      const rootValue = {};
-      const context = {};
+      const rootValue = {}
+      const context = {}
 
-      const result = await graphql(schema, query, rootValue, context);
-      const { data } = result;
+      const result = await graphql(schema, query, rootValue, context)
+      const { data } = result
 
-      expect(data.viewer).toBe(null);
-    });
+      expect(data.viewer).toBe(null)
+    })
 
     it('should return the current user when user is logged in', async () => {
-      const user = Database.getUser('1');
+      const user = Database.getUser('1')
 
       const query = `
         query {
@@ -46,29 +46,29 @@ describe('GraphQL', () => {
             }
           }
         }
-      `;
+      `
 
-      const rootValue = {};
-      const context = { user };
+      const rootValue = {}
+      const context = { user }
 
-      const result = await graphql(schema, query, rootValue, context);
-      const { data } = result;
+      const result = await graphql(schema, query, rootValue, context)
+      const { data } = result
 
-      expect(data.viewer.name).toBe(user.name);
-      expect(data.viewer.email).toBe(user.email);
-      expect(data.viewer.role).toBe(user.role);
-      expect(data.viewer.website).toBe(user.website);
-      expect(data.viewer.stories.edges.length).toBe(4);
-    });
-  });
+      expect(data.viewer.name).toBe(user.name)
+      expect(data.viewer.email).toBe(user.email)
+      expect(data.viewer.role).toBe(user.role)
+      expect(data.viewer.website).toBe(user.website)
+      expect(data.viewer.stories.edges.length).toBe(4)
+    })
+  })
 
   describe('StoryType', () => {
-    const user = Database.getUser('1');
+    const user = Database.getUser('1')
 
     it('returns the correct story associated with a user ', async () => {
-      const storyId = toGlobalId('Story', '1');
+      const storyId = toGlobalId('Story', '1')
 
-      const variables = { storyId };
+      const variables = { storyId }
       const query = `
         query($storyId: String!) {
           viewer {
@@ -77,15 +77,15 @@ describe('GraphQL', () => {
             }
           }
         }
-      `;
+      `
 
-      const rootValue = {};
-      const context = { user };
+      const rootValue = {}
+      const context = { user }
 
-      const result = await graphql(schema, query, rootValue, context, variables);
-      const { data } = result;
+      const result = await graphql(schema, query, rootValue, context, variables)
+      const { data } = result
 
-      expect(data.viewer.story.id).toBe(storyId);
-    });
-  });
-});
+      expect(data.viewer.story.id).toBe(storyId)
+    })
+  })
+})
