a265449c2c28173880dda444a1fd904eda0f0729
Jon Stokes <jon@jonstokes.com>
2017-04-08 13:41:06 -0500

    Trying to add morgan

--- hoverBoard/app.js
+++ hoverBoard/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import flash from 'connect-flash'
 import session from 'express-session'
+import morgan from 'morgan'
 import graphQLHTTP from 'express-graphql'
 import bodyParser from 'body-parser'
 import uuid from 'node-uuid'
@@ -9,6 +10,7 @@ import { Strategy } from 'passport-local'
 import path from 'path'
 import chalk from 'chalk'
 import _ from 'lodash'
+import fs from 'fs'
 
 import Logger from 'hoverBoard/logger'
 import schema from 'config/schema'
@@ -42,6 +44,9 @@ export default class App {
       genid: (req) => uuid.v4(),
       secret: this.secret
     }))
+
+    const accessLogStream = fs.createWriteStream(`log/${process.env.NODE_ENV}.log`, {flags: 'a'})
+    server.use(morgan('combined', {stream: accessLogStream}))
 
     // This is for passport
     server.use(flash())
